21aca60e65059bd23c7349b7a9643f36b804122e
Gulnisa Huseynli <guulnisah@gmail.com>
2022-09-04 02:29:22 +0400

    add the right way

--- 8-classes/myMap/script.js
+++ 8-classes/myMap/script.js
@@ -4,16 +4,21 @@ class myMap {
     }
 
     set(key, value) {
-        const obj = {
-            key: key,
-            value: value
+        let hasKey = this.array.some((elem) => {
+            if (Object.keys(elem)[0] === key) {
+                elem[key] = value;
+                return true;
+            }
+        })
+        if (!hasKey) {
+            this.array.push({ [key]: value });
         }
-        this.array.push(obj)
+        return this.array;
     }
 
     delete(key) {
         for (let i = 0; i < this.array.length; i++) {
-            if (this.array[i].key === key) {
+            if (this.array[i][key]) {
                 this.array.splice(i, 1);
             }
         }
@@ -21,7 +26,7 @@ class myMap {
 
     get(key) {
         for (let elem of this.array) {
-            if (elem.key === key) {
+            if (elem[key]) {
                 return elem
             }
         }
@@ -30,12 +35,13 @@ class myMap {
     has(key) {
         let hasKey = false
         for (let i = 0; i < this.array.length; i++) {
-            if (this.array[i].key === key) {
+            if (this.array[i][key]) {
                 hasKey = true
             }
         }
         return hasKey
     }
+
 
     clear() {
         this.array = {}
@@ -46,9 +52,59 @@ class myMap {
     }
 }
 
+
+// class myMap {
+//     constructor() {
+//         this.array = []
+//     }
+
+//     set(key, value) {
+//         const obj = {
+//             key: key,
+//             value: value
+//         }
+//         this.array.push(obj)
+//     }
+
+//     delete(key) {
+//         for (let i = 0; i < this.array.length; i++) {
+//             if (this.array[i].key === key) {
+//                 this.array.splice(i, 1);
+//             }
+//         }
+//     }
+
+//     get(key) {
+//         for (let elem of this.array) {
+//             if (elem.key === key) {
+//                 return elem
+//             }
+//         }
+//     }
+
+//     has(key) {
+//         let hasKey = false
+//         for (let i = 0; i < this.array.length; i++) {
+//             if (this.array[i].key === key) {
+//                 hasKey = true
+//             }
+//         }
+//         return hasKey
+//     }
+
+//     clear() {
+//         this.array = {}
+//     }
+
+//     get size() {
+//         return this.array.length
+//     }
+// }
+
 const newMap = new myMap()
 console.log(newMap)
 newMap.set('one', 'uno')
 newMap.set('two', 'dos')
 newMap.set('three', 'tres')
 newMap.set('four', 'cuatro')
+newMap.set('one', 'une')
